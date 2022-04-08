a372245402a024f1aaec558699c1e633cc8fe3e6
Darwin073100 <edwinegq@outlook.com>
2022-04-07 20:42:09 -0500

    Frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <span class="h2">Contenido Mas Popular</span>
+        <div class="col">
+            <px-card/>
+        </div>
+        <div class="col">
+            <px-card/>
+        </div>
+        <div class="col">
+            <px-card/>
+        </div>
+        <div class="col">
+            <px-card/>
+        </div>
+    </div>
+</template>
+<script>
+import PxCard from '@/components/user/PxCard.vue';
+
+export default {
+  components: { PxCard },
+    name:'viewHome'
+}
+</script>
